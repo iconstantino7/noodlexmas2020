d92521817002391abef515324113032d22ae0e1e
Ivan Constantino <ivan@mars-websites.com>
2020-12-24 18:34:20 -0800

    second

---
+++ AudreyXmas2020/Pages/Reindeer.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AudreyXmas2020.Pages
+{
+    public class ReindeerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
